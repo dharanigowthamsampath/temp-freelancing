ec4568961a7906b34458fdf746c3be55ad171c8c
dharanigowthamsampath <dharanigowthamsampath@gmail.com>
2024-07-12 10:36:37 +0530

    Added UI

--- components/job-form.tsx
+++ components/job-form.tsx
@@ -11,7 +11,6 @@ import {
 } from "./ui/card";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-import { Label } from "./ui/label";
 import {
   Select,
   SelectContent,
